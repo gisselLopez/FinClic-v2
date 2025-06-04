8b853c96bd751f39763350bec2f2900f18ffa982
Karla Gissel Lopez <65907889+gisselLopez@users.noreply.github.com>
2025-06-03 20:46:55 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   BrowserRouter as Router,
+  
   Routes,
   Route,
 
@@ -10,21 +11,29 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import "bootstrap/dist/css/bootstrap.min.css";
 import RegistroForm from "./componentes/Login/RegistroForm";
 import InicioSesion from "./componentes/Login/InicioSesion";
-import Navbar from "./componentes/Navbar/Header";
 import Cliente from "./componentes/Cliente/cliente";
 import ListarCliente from "./componentes/Cliente/ListarClientes";
 import RegistrarCredito from "./componentes/credito/RegistrarCredito"
 import  ListarCreditosPorCliente from "./componentes/credito/ListarCreditosPorCliente";
-import CreditosEstadisticas from "./componentes/Estadisticas/CreditosPorMes";
-import Detallecredito from "./componentes/detalleCredito/detallecredito"
+import Dashboard from "./componentes/Estadisticas/Dashboard";
+import ListarCredito from "./componentes/detalleCredito/listarCredito"
+import DetalleCredito from "./componentes/detalleCredito/TablaAmortizacion";
 import Pagos from "./componentes/pagos/pagos";
+import Abonos from "./componentes/Abonos/abonos";
 import Home from "./componentes/Home/Home";
 import Layout from "./componentes/Sidebar/Layout";
-import useAuth from "./hooks/useAuth"; // Importa el hook useAuth
+
+import RecuperarContrasena from "./componentes/RecuperarContrasena/RecuperarContra"
+import RestablecerContrasena from "./componentes/RecuperarContrasena/restablecerContra";
+import ListarUsuarios from "./componentes/Login/listarUsuarios";
+import ModificarUsuario from "./componentes/Login/modificarUsuario";
 import ProtectedWrapper from "./componentes/Auth/Providers/ProtectedWrapper";
+import {useAuth} from "./hooks/AuthContext"; 
 
 const App = () => {
-  const { handleLoginSuccess, isLoggedIn } = useAuth(); // Utiliza el hook useAuth
+
+  const { handleLoginSuccess} = useAuth(); 
+
   const queryClient = new QueryClient();
   
   return (
@@ -37,22 +46,43 @@ const App = () => {
             element={<InicioSesion onLoginSuccess={handleLoginSuccess} />}
           />
           <Route path="/registro" element={<RegistroForm />} />
+          <Route
+            path="/recuperar-contrasena"
+            element={<RecuperarContrasena />}
+          />
+          <Route
+            path="/restablecer-contrasena"
+            element={<RestablecerContrasena />}
+          />
+
           {/* Rutas protegidas */}
           <Route path="/dashboard/*" element={<Layout />}>
-           
-              {/* Rutas secundarias dentro de /dashboard */}
-              <Route path="home" element={<Home />} />
-              <Route path="crear" element={<Cliente />} />
-              <Route path="registrar-credito" element={<RegistrarCredito />} />
-              <Route path="registrar-Pagos" element={<Pagos />} />
-              <Route path="detallecredito" element={<Detallecredito />} />
-              <Route
-                path="listarCreditoPorcliente"
-                element={<ListarCreditosPorCliente />}
-              />
-              <Route path="listarcliente" element={<ListarCliente />} />
-              <Route path="estadisticas" element={<CreditosEstadisticas />} />
-            
+            {/* Rutas secundarias dentro de /dashboard */}
+
+            <Route path="home" element={<Home />} />
+            <Route path="crear" element={<Cliente />} />
+            <Route path="registrar-credito" element={<RegistrarCredito />} />
+            <Route path="registrar-Pagos" element={<Pagos />} />
+            <Route path="registrar-Abonos" element={<Abonos />} />
+            <Route path="lista-creditos" element={<ListarCredito />} />
+            <Route
+              path="listarCreditoPorcliente"
+              element={<ListarCreditosPorCliente />}
+            />
+            <Route path="listarcliente" element={<ListarCliente />} />
+            <Route path="listarUsuario" element={<ListarUsuarios />} />
+            <Route
+              path="modificarUsuario/:idRegistro"
+              element={<ModificarUsuario />}
+            />
+
+            <Route path="Dashboard/estadisticas" element={<Dashboard />} />
+
+            {/* Ruta para el detalle del crédito */}
+            <Route
+              path="creditos/:id/:idCliente"
+              element={<DetalleCredito />}
+            />
           </Route>
         </Routes>
       </Router>
@@ -63,90 +93,3 @@ const App = () => {
 export default App;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- <Router>
-          <Routes>
-            {/* Rutas para la página de inicio de sesión y registro /}
-            <Route
-              path="/inicio-sesion"
-              element={<InicioSesion onLoginSuccess={handleLoginSuccess} />}
-            />
-            <Route path="/registro" element={<RegistroForm />} />
-            <Route path="crear" element={<Cliente />} />
-
-            {/* Rutas protegidas /}
-            {isLoggedIn ? (
-              <>
-                {/* Rutas dentro del Layout /}
-                <Route path="/dashboard/*" element={<Layout />}>
-                  <Route index element={<Home />} />
-                  <Route path="listarcliente" element={<ListarCliente />} />
-                </Route>
-              </>
-            ) : (
-              // Redirigir a /inicio-sesion si el usuario no está autenticado
-              <Route
-                path="*"
-                element={<Navigate to="/inicio-sesion" replace />}
-              />
-            )}
-          </Routes>
-        </Router>
- 
- */
-
-
-
-
-
-/*
-   
-      {/* Proporciona el QueryClient en el QueryClientProvider /}
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Routes>
-            {/* Rutas para la página de inicio de sesión y registro /}
-            <Route
-              path="/inicio-sesion"
-              element={<InicioSesion onLoginSuccess={handleLoginSuccess} />}
-            />
-            <Route path="/registro" element={<RegistroForm />} />
-            <Route path="/registrar-credito" element={<Credito />} />
-
-            {/* Rutas protegidas /}
-            {isLoggedIn ? (
-              <>
-                {/* Rutas dentro del Layout /}
-                <Route path="/dashboard/*" element={<Layout />}>
-                  <Route index element={<Home />} />
-                  <Route path="crear" element={<Cliente />} />
-                  <Route path="listarcliente" element={<ListarCliente />} />
-                </Route>
-              </>
-            ) : (
-              // Redirigir a /inicio-sesion si el usuario no está autenticado
-              <Route
-                path="*"
-                element={<Navigate to="/inicio-sesion" replace />}
-              />
-            )}
-          </Routes>
-        </Router>
-      </QueryClientProvider>
-    
-*/
